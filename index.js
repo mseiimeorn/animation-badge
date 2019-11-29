edb2db21b4b7d95f26580db8064b719205906d65
Simon Meier <simon.meier@netcentric.biz>
2019-11-29 22:26:47 +0100

    working with css

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const netcentric = document.querySelector('.NC');
+var netcentric = document.querySelector('.NC');
+var letters = document.querySelectorAll('.letter');
 
-var stopAnimation= function () {
+function stopAnimation() {
     netcentric && netcentric.querySelector('path') && netcentric.querySelector('path').classList.remove('animated');
+    Array.from(letters).forEach(function(letter) {
+        letter.classList.remove('letter-animation');;
+    });
 }
 
-var startAnimation = function () {
+function startAnimation() {
     netcentric && netcentric.querySelector('path') && netcentric.querySelector('path').classList.add('animated');
+    Array.from(letters).forEach(function(letter) {
+        letter.classList.add('letter-animation');;
+    });
 }
 
 (function () {
+    Array.from(letters).forEach(function(letter) {
+        letter.classList.add('letter-animation');
+    });
     netcentric.addEventListener('mouseover', stopAnimation);
     netcentric.addEventListener('mousemove', stopAnimation);
     netcentric.addEventListener('mouseleave', startAnimation);
